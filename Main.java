ab3d5025125dd443b023583f940834246042467c
Recaedae <lemour@live.co.uk>
2021-08-02 12:20:57 +0200

    Removed redundant files.

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        FirstRatings fileR = new FirstRatings();
-
-        // testLoadMovies
-        fileR.testLoadMovies();
-    }
-}
--- Tester.java
+++ Tester.java
@@ -1,28 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
-        MovieRunnerSimilarRatings movieRun = new MovieRunnerSimilarRatings();
         RecommendationRunner recoRun = new RecommendationRunner();
-
-        // // test printAverageRatings 
-        // movieRun.printAverageRatings();
-
-        // // test printAverageRatingsByYearAfterAndGenre 
-        // movieRun.printAverageRatingsByYearAfterAndGenre();
-
-        // // test printSimilarRatings 
-        // movieRun.printSimilarRatings();
-
-        // // test printSimilarRatingsByGenre 
-        // movieRun.printSimilarRatingsByGenre();
-
-        // // test printSimilarRatingsByDirector  
-        // movieRun.printSimilarRatingsByDirector();
-
-        // // test printSimilarRatingsByGenreAndMinutes   
-        // movieRun.printSimilarRatingsByGenreAndMinutes();
-
-        // // test printSimilarRatingsByYearAfterAndMinutes   
-        // movieRun.printSimilarRatingsByYearAfterAndMinutes();
 
         // test printSimilarRatingsByYearAfterAndMinutes   
         recoRun.printRecommendationsFor("167");
